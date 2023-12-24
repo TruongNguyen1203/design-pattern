ed7f8d05b38d6612824eebf149f39721a100fcee
Truong Nguyen <truongnguyen@192.168.1.12>
2023-12-24 14:39:01 +0700

    singleton

---
+++ DesignPattern/Singleton/Program.cs
@@ -0,0 +1,54 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+public sealed class Singleton
+{
+    private Singleton(){}
+
+    private static Singleton _instance;
+    private static readonly object _lock = new object();
+    public string Value { get; set; }
+
+    public static Singleton GetInstance(string value)
+    {
+        if (_instance == null)
+        {
+            lock (_lock)
+            {
+                if (_instance == null)
+                {
+                    _instance = new Singleton();
+                    _instance.Value = value;
+                }
+            }
+        }
+        return _instance;
+    }
+}
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Thread thread1 = new Thread(() =>
+        {
+            TestSingleton("One");
+        });
+
+        Thread thread2 = new Thread(() =>
+        {
+            TestSingleton("Two");
+        });
+
+        thread1.Start();
+        thread2.Start();
+
+        thread1.Join();
+        thread2.Join();
+    }
+
+    private static void TestSingleton(string value)
+    {
+        var instane = Singleton.GetInstance(value);
+        Console.WriteLine(instane.Value);
+    }
+}
